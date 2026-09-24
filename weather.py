c25e5ed19385e7d16eee99f289c095f55509043f
nanducs507715 <nanthanasankar@gmail.com>
2026-09-24 23:01:35 +0530

    History feature added

--- weather.py
+++ weather.py
@@ -1,4 +1,6 @@
 import requests
+import csv
+from datetime import date
 
 api_key = "bfd877a695803bcaf596da0aa09e42c9"
 api_l = "http://api.openweathermap.org/data/2.5/weather"  # current weather endpoint
@@ -22,5 +24,34 @@ Temperature: {data["main"]["temp"]}
 Weather: {data["weather"][0]["description"]}
     """)
 
-city = input("Enter your city: ")
-get_weather(city)
+
+    with open("Weather_history.csv","a",newline="") as f:
+        w = csv.writer(f)
+        w.writerow([city,data["main"]["temp"],data["weather"][0]["description"], date.today()])
+        f.close()
+
+def history():
+    f = open("Weather_history.csv","r")
+    r = csv.reader(f)
+    for i in r:
+        print(i)
+    f.close()
+    
+
+print("""Choice: 
+    1. View weather
+    2. Check old Records
+    3. Exit
+    """)
+while True:
+    _choice = int(input("Choice: "))
+    if _choice==1:
+        city = input("Enter your city: ")
+        get_weather(city)
+    elif _choice==2:
+        history()
+    elif _choice==3:
+        break
+    else:
+        print("Wrong Choice")
+    print("----------------------------------------")
